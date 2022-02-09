f549ad24524ab9789d7e0739c152f4bfe9e74609
Luis Perez <le.perezl@uniandes.edu.co>
2022-02-09 16:58:07 -0500

    not wokring e1

--- A1e1LP.exs
+++ A1e1LP.exs
@@ -1,3 +1,10 @@
 defmodule Task1_P1 do
-    def kind(a,b,c)
+    def kind(a,b,c) do
+        case {a,b,c} do
+            {a,b,c} when( (a + b>= c) and (a+c >= b) and (b+c >=a))->
+                "heyy"
+            {a,b,c} -> {:error,String.t(woa buddy)}
+        end
+        
+    end
 end
