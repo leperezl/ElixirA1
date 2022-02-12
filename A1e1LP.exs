e419242d32edcad50f6b081dc540366e7c0e5b94
Luis Perez <le.perezl@uniandes.edu.co>
2022-02-12 16:35:56 -0500

    task1 complete

--- A1e1LP.exs
+++ A1e1LP.exs
@@ -2,9 +2,20 @@ defmodule Task1_P1 do
     def kind(a,b,c) do
         case {a,b,c} do
             {a,b,c} when( (a + b>= c) and (a+c >= b) and (b+c >=a))->
-                "heyy"
-            {a,b,c} -> {:error,String.t(woa buddy)}
+                #kind1(a,b,c)
+                kind1(a,b,c)
+            {a,b,c} -> {:error,'woa buddy, not a triangle'}
         end
         
+    end
+
+    def kind1(a,b,c) do
+        case {a,b,c} do
+            {a,a,a} -> {:ok, 'equilateral'}
+            {a,a,_} -> {:ok, 'isosceles'}
+            {a,_,a} -> {:ok, 'isosceles'}
+            {_,b,b} -> {:ok, 'isosceles'}
+            {_,_,_} -> {:ok, 'escalene'}
+        end
     end
 end
