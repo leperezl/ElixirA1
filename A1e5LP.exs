e183ab19c59e0921973ccf77751b83884ccc1b58
Luis Perez <le.perezl@uniandes.edu.co>
2022-02-14 14:21:46 -0500

    5 is working p well, add/remove

--- A1e5LP.exs
+++ A1e5LP.exs
@@ -6,6 +6,33 @@ defmodule Store do
     def add_item(inv, item) do
         [item | inv]
     end
+
+    def add_size(inv,name, size, quantity) do
+        item = search(inv,name)
+        inv = List.delete(inv,item)
+        itemF = Map.update!(item, :quantity_by_size, &(Map.put(&1, size, quantity)))
+        [itemF | inv]
+    end
+
+    def remove_size(inv, name, size) do
+        item = search(inv,name)
+        inv = List.delete(inv,item)
+        itemF = Map.update!(item, :quantity_by_size, &(Map.delete(&1, size)))
+        [itemF | inv]
+    end
+    
+
+    def search([], name) do
+        :no_item
+    end
+
+    def search([h|t], name) do
+        cond do
+            name == Map.get(h,:name) -> h
+            true -> search(t,name)
+        end
+    end
+
 end
 
 #%{name: "shirt", price: 40, quantity_by_size: %{L: 7, s: 5}}
--- garb.exs
+++ garb.exs
@@ -8,3 +8,5 @@ String.splitter("",trim:true)
 
 nums = {1,2,3,4,5,6}
 Binar.search({1,2,3,4,5,6},4)
+
+inv = [%{name: "shirt", price: 999, quantity_by_size: %{s: 3, m: 5}}, %{name: "pant", price: 85, quantity_by_size: %{s: 4, m: 6}}, %{name: "shoe", price: 250, quantity_by_size: %{s: 5, l: 2}}]
