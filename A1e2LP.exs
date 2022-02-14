0a8397f32989daec73659036e60a3bc7c607dae1
Luis Perez <le.perezl@uniandes.edu.co>
2022-02-13 20:29:57 -0500

    3 and 1 work

--- A1e2LP.exs
+++ A1e2LP.exs
@@ -1,2 +1,19 @@
 defmodule Robot do
+    def create(a,{b,c}) do
+        case {a, {b,c}} do 
+            {a, {b,c}} when  is_integer(b) and is_integer(c) ->
+                create2(a, {b,c})
+            {a, {b,c}} -> {:error, "not valid, coordenates need to be integers"}
+        end
+    end
+    
+    def create2(a, {b,c}) do
+        case {a, {b,c}} do
+            {a, {b,c}} -> {:north, {b,c}} 
+            {a, {b,c}} -> {:south, {b,c}} 
+            {:east, {b,c}} -> {:east, {b,c}}
+            {:west, {b,c}} -> {:west, {b,c}}
+            {_, {b,c}}->{:error, "not valid, direction needs to be correct"}
+        end
+    end
 end
---
+++ garb.exs
@@ -0,0 +1,4 @@
+#add one to a key if the key exists
+map = Map.put(map, "key", Map.get(res, "key")+1)
+#if the key doesnt exist
+map = Map.put(map, "key", 1)
